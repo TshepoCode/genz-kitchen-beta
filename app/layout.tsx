e74bd609698f1c14411d70a61ef85383e7c8125a
IrvinMashile <irvingamer40@gmail.com>
2026-05-18 22:14:12 +0200

    Fix production build

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { CartProvider } from "./context/CartContext";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   icons: {
@@ -29,7 +18,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className="font-sans"
     >
       <body className="h-auto overflow-x-hidden flex flex-col">
         <CartProvider>{children}</CartProvider>
